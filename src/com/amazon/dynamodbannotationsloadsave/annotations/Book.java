1ddde44aac9b40c75480b47f1da3c7ae1308166b
Santiago Coria <santiago_coria@hotmail.com>
2024-06-14 14:43:34 -0300

    Book

--- src/com/amazon/dynamodbannotationsloadsave/annotations/Book.java
+++ src/com/amazon/dynamodbannotationsloadsave/annotations/Book.java
@@ -1,6 +1,61 @@
 package com.amazon.dynamodbannotationsloadsave.annotations;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
+
+
+@DynamoDBTable(tableName = "DynamoDbAnnotationsLoadSave-Books")
 public class Book {
 
+    private String asin;
+    private String author;
+    private String title;
+    private int year_published;
 
+    @DynamoDBHashKey(attributeName = "ASIN")
+    public String getAsin() {
+        return asin;
+    }
+
+    public void setAsin(String asin) {
+        this.asin = asin;
+    }
+
+    @DynamoDBAttribute(attributeName = "Title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @DynamoDBAttribute(attributeName = "Author")
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @DynamoDBAttribute(attributeName = "PublicationYear")
+    public int getYear_published() {
+        return year_published;
+    }
+
+    public void setYear_published(int year_published) {
+        this.year_published = year_published;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "asin='" + asin + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publicationYear=" + year_published +
+                '}';
+    }
 }
+
+
